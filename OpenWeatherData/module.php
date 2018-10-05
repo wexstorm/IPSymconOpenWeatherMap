7b95fb2e5d53f4bec6ad38f0bb5636b5934df246
demel42 <demel42@web.de>
2018-10-05 23:19:57 +0200

    Zugriff auf _ENV['LANG']] abgesichert

--- OpenWeatherData/module.php
+++ OpenWeatherData/module.php
@@ -216,8 +216,11 @@ class OpenWeatherData extends IPSModule
                 'units' => 'metric'
             ];
 
-        if (preg_match('/([a-z]*)_.*/', $_ENV['LANG'], $r)) {
-            $args['lang'] = $r[1];
+		is (isset($_ENV['LANG'])) {
+			$lang = $_ENV['LANG'];
+        	if (preg_match('/([a-z]*)_.*/', $lang, $r)) {
+            	$args['lang'] = $r[1];
+        	}
         }
 
         $jdata = $this->do_HttpRequest('data/2.5/weather', $args);
