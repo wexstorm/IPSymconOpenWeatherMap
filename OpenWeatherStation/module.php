d7b8f11a0ace20e83de8db1bf07b10efd1d0cd1c
IP-Symcon <root@ips-prod>
2019-01-29 17:03:53 +0100

    v1.12

--- OpenWeatherStation/module.php
+++ OpenWeatherStation/module.php
@@ -361,17 +361,22 @@ class OpenWeatherStation extends IPSModule
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_TIMEOUT, 30);
         $cdata = curl_exec($ch);
+		$cerrno = curl_errno ($ch);
+		$cerror = $cerrno ? curl_error($ch) : '';
         $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
         curl_close($ch);
 
         $duration = round(microtime(true) - $time_start, 2);
-        $this->SendDebug(__FUNCTION__, ' => httpcode=' . $httpcode . ', duration=' . $duration . 's', 0);
+        $this->SendDebug(__FUNCTION__, ' => errno=' . $cerrno . ', httpcode=' . $httpcode . ', duration=' . $duration . 's', 0);
         $this->SendDebug(__FUNCTION__, '    cdata=' . $cdata, 0);
 
         $statuscode = 0;
         $err = '';
         $result = '';
-        if ($httpcode < 200 || $httpcode > 299) {
+        if ($cerrno) {
+            $statuscode = IS_INVALIDDATA;
+            $err = 'got curl-errno ' . $cerrno . ' (' . $cerror . ')';
+        } elseif ($httpcode < 200 || $httpcode > 299) {
             if ($httpcode >= 500 && $httpcode <= 599) {
                 $statuscode = IS_SERVERERROR;
                 $err = "got http-code $httpcode (server error)";
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # IPSymconOpenWeatherMap
 
 [![IPS-Version](https://img.shields.io/badge/Symcon_Version-5.0-red.svg)](https://www.symcon.de/service/dokumentation/entwicklerbereich/sdk-tools/sdk-php/)
-![Module-Version](https://img.shields.io/badge/Modul_Version-1.11-blue.svg)
+![Module-Version](https://img.shields.io/badge/Modul_Version-1.12-blue.svg)
 ![Code](https://img.shields.io/badge/Code-PHP-blue.svg)
 [![License](https://img.shields.io/badge/License-CC%20BY--NC--SA%204.0-green.svg)](https://creativecommons.org/licenses/by-nc-sa/4.0/)
 [![StyleCI](https://github.styleci.io/repos/126683101/shield?branch=master)](https://github.styleci.io/repos/150288134)
@@ -78,6 +78,9 @@ Verweise:
 
 
 ## 7. Versions-Historie
+
+- 1.12 @ 23.01.2019 18:18<br>
+  - curl_errno() abfragen
 
 - 1.11 @ 22.12.2018 12:10<br>
   - Fehler in der http-Kommunikation nun nicht mehr mit _echo_ (also als **ERROR**) sondern mit _LogMessage_ als **NOTIFY**
