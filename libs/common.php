3bafe7b696af2f34e2ce795a7205d7adc1f4062b
IP-Symcon <root@ips-prod>
2019-08-09 14:47:15 +0200

    Schreibfehler korrigiert

--- libs/common.php
+++ libs/common.php
@@ -43,7 +43,7 @@ trait OpenWeatherMapCommon
         return $ret;
     }
 
-    private function CreateVarProfile($Name, $ProfileType, $Suffix, $MinValue, $MaxValue, $StepSize, $Digits, $Icon, $Asscociations = '')
+    private function CreateVarProfile($Name, $ProfileType, $Suffix, $MinValue, $MaxValue, $StepSize, $Digits, $Icon, $Associations = '')
     {
         if (!IPS_VariableProfileExists($Name)) {
             IPS_CreateVariableProfile($Name, $ProfileType);
@@ -51,8 +51,8 @@ trait OpenWeatherMapCommon
             IPS_SetVariableProfileValues($Name, $MinValue, $MaxValue, $StepSize);
             IPS_SetVariableProfileDigits($Name, $Digits);
             IPS_SetVariableProfileIcon($Name, $Icon);
-            if ($Asscociations != '') {
-                foreach ($Asscociations as $a) {
+            if ($Associations != '') {
+                foreach ($Associations as $a) {
                     $w = isset($a['Wert']) ? $a['Wert'] : '';
                     $n = isset($a['Name']) ? $a['Name'] : '';
                     $i = isset($a['Icon']) ? $a['Icon'] : '';
